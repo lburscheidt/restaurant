3231ffe7bc2a422a1aef383c5ab55641bf4c48cc
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-23 13:01:40 +0200

    feat: create main page in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import "./styles.css";
-// import { greeting } from "./menu.js";
-//
-// console.log(greeting);
-//
-// const image = document.createElement("img");
-// image.src = odinImage;
-//
-// document.body.appendChild(image);
+import udon from "./udon.jpg";
 console.log("hello");
+
+let content = document.querySelector("#content");
+
+let heading = document.createElement("h1");
+heading.textContent = "Naomi's Noodles";
+let tagline = document.createElement("p");
+tagline.textContent = "We're the home of authentic Japanese cuisine.";
+
+let figure = document.createElement("figure");
+let image = document.createElement("img");
+image.src = udon;
+image.className = "food";
+let figcaption = document.createElement("figcaption");
+figcaption.textContent = "Our signature udon noodles";
+content.appendChild(heading);
+content.appendChild(tagline);
+content.appendChild(figure);
+figure.appendChild(image);
+figure.appendChild(figcaption);
