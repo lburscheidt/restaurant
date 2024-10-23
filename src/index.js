e9f76d31e738626593ce79e07fac0bf5a5df5a18
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-23 12:19:05 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+// import { greeting } from "./menu.js";
+//
+// console.log(greeting);
+//
+// const image = document.createElement("img");
+// image.src = odinImage;
+//
+// document.body.appendChild(image);
+console.log("hello");
